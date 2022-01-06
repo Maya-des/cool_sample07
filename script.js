7a2743009539d20e328e771503b2d671c10bc606
Maya-des <64457837+Maya-des@users.noreply.github.com>
2022-01-06 17:58:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ $(function() {
     $(".lists").slideToggle();
   });
 
-  //$('#MenuButton').on('click', function () {
-  //$(this).toggleClass('open');
-  //$('#MenuList').slideToggle();
-  //});
+  $('#MenuButton').on('click', function () {
+  $(this).toggleClass('open');
+  $('#MenuList').slideToggle();
+  });
 
-  //$('#MenuList a').on('click', function () {
-  //if( window.innerWidth <= 768){
-  //$('#MenuButton').removeClass('open');
-  //$('#MenuList').slideToggle();
-  //}
-  //});
+  $('#MenuList a').on('click', function () {
+  if( window.innerWidth <= 768){
+  $('#MenuButton').removeClass('open');
+  $('#MenuList').slideToggle();
+  }
+  });
 
   // Window Resize Function
 
